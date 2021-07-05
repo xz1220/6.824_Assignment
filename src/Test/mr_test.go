9bf97f3d36962b8ab782e5d5e62e849d0cb59a25
xingzheng.dan <xingzheng.dan@bytedance.com>
2021-07-05 21:48:11 +0800

    feature: try to implement file lock

--- src/Test/mr_test.go
+++ src/Test/mr_test.go
@@ -12,7 +12,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+	"syscall"
 	"testing"
+	"time"
 )
 
 func TestOSPackage(t *testing.T) {
@@ -60,6 +63,69 @@ func TestOSPackage(t *testing.T) {
 	file.Write([]byte("ttttttttt"))
 	file.Sync()
 	file.Close()
+}
+
+func TestForFileLock(t *testing.T) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Printf("os Getwd Err")
+		panic(err)
+	}
+	fmt.Println("pwd is - ", pwd)
+
+	TestFile := pwd + "/" + "test.txt"
+	var file *os.File
+	if checkFileIsExist(TestFile) {
+		file, err = os.OpenFile(TestFile, os.O_APPEND, 0666)
+		if err != nil {
+			log.Printf("file create err")
+		}
+		// defer file.Close()
+	} else {
+		file, err = os.Create(TestFile)
+		if err != nil {
+			log.Printf("file create err")
+		}
+		// defer file.Close()
+	}
+	defer file.Close()
+
+	// add lock to file
+	// // 非阻塞模式下，加共享锁
+	// if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH|syscall.LOCK_NB); err != nil {
+	// 	log.Println("add share lock in no block failed", err)
+	// }
+	// // 这里进行业务逻辑
+	// // TODO
+
+	// // 解锁
+	// if err := syscall.Flock(int(file.Fd()), syscall.LOCK_UN); err != nil {
+	// 	log.Println("unlock share lock failed", err)
+	// }
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 2; i++ {
+		wg.Add(1)
+
+		go func(*os.File) {
+			if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
+				log.Println("add share lock in no block failed", err)
+			}
+
+			time.Sleep(10 * time.Second)
+
+			// 解锁
+			if err := syscall.Flock(int(file.Fd()), syscall.LOCK_UN); err != nil {
+				log.Println("unlock share lock failed", err)
+			}
+
+		}(file)
+	}
+
+	wg.Wait()
+
+	
 }
 
 /*
